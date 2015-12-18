8d6aa9423fbee25f4bc47e36d866f04578656038
SamEureka <sameureka@me.com>
2015-12-17 23:13:15 -0500

    added init function to Profile.js, it's all working

--- app/components/notes/AddNote.js
+++ app/components/notes/AddNote.js
@@ -18,7 +18,7 @@ var AddNote = React.createClass({
 			<div className="input-group">
 				<input type="text" className="form-control" placeholder="Add New Note" ref={this.setRef} />
 				<span className="input-group-btn">
-				<button className="btn btn-default" type="button" onClick={this.handleSubmit}>Submit</button>
+					<button className="btn btn-default" type="button" onClick={this.handleSubmit}>Submit</button>
 				</span>
 			</div> 
 		)
